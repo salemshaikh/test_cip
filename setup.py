f742784ef8f96abec56408f01ca67cdbdc02d0f4
salemshaikh <salem.shaikh@cipla.com>
2024-01-10 16:27:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cip_api',
+    name='cip',
     version='0.1',
     packages=find_packages(),
     description='A simple example Python package',
